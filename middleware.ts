7535725ea3fb3294dccbe592f17d5ee4e56faf44
imran hassan <hassanimran114@gmail.com>
2024-11-21 22:12:25 +0530

    first commit

---
+++ middleware.ts
@@ -0,0 +1,23 @@
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+
+const protectedRoute = createRouteMatcher([
+  '/',
+  '/upcoming',
+  '/meeting(.*)',
+  '/previous',
+  '/recordings',
+  '/personal-room',
+]);
+
+export default clerkMiddleware(async (auth, req) => {
+  if (protectedRoute(req)) {
+    const session = await auth();
+    if (!session.userId) {
+      throw new Error("Unauthorized");
+    }
+  }
+});
+
+export const config = {
+  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+};
